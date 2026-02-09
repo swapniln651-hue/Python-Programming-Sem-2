02ddd21ef12414b0254c5483c83aa9a4ad21cd44
swapniln651-hue <swapniln651@gmail.com>
2026-02-09 15:06:46 +0530

    Add files via upload

---
+++ Expt1/Act123.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  9 09:28:37 2026
+
+@author: User
+"""
+
+num1=int(input("Enter Number1:"))
+num2=int(input("Enter Number2:"))
+
+sum=num1+num2
+print("Addition=",sum)
