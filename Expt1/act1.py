9fb25246b5e4dbfb9cbd3c7542e1f31482f765ac
swapniln651-hue <swapniln651@gmail.com>
2026-02-09 15:25:22 +0530

    Update act1.py

--- Expt1/act1.py
+++ Expt1/act1.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb  9 09:38:15 2026
 
-@author: User
-"""
 
 num1=int(input("Enter Number1:"))
 num2=int(input("Enter Number2:"))
@@ -11,26 +6,14 @@ num2=int(input("Enter Number2:"))
 sum=num1+num2
 print("Addition=",sum)
 
-num1=int(input("Enter Number1:"))
-num2=int(input("Enter Number2:"))
-
 sub=num1-num2
 print("Subraction=",sub) 
-
-num1=int(input("Enter Number1:"))
-num2=int(input("Enter Number2:"))
 
 mult=num1*num2
 print("Multiplication=",mult)
 
-num1=int(input("Enter Number1:"))
-num2=int(input("Enter Number2:"))
-
 div=num1/num2
 print("Division=",div)
-
-num1=int(input("Enter Number1:"))
-num2=int(input("Enter Number2:"))
 
 mod=num1%num2
 print("Modulus=",mod)
