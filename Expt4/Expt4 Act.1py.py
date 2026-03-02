96c196d5f97ca6420363d1c6522d0e3c899814e9
swapniln651-hue <swapniln651@gmail.com>
2026-03-02 15:47:37 +0530

    Update Expt4 Act.1py.py

--- Expt4/Expt4 Act.1py.py
+++ Expt4/Expt4 Act.1py.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# find average and topper marks
 """
 Created on Mon Mar  2 15:34:18 2026
 
@@ -12,4 +12,5 @@ average_marks =  sum(marks) / len(marks)
 topper_marks = max(marks)
 
 print(f"Average marks: {average_marks:.2f}")
+
 print(f"Topper marks: {topper_marks}")
