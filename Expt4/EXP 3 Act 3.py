36dc0ff9755dc9077bbd83d68eb750af807040fe
swapniln651-hue <swapniln651@gmail.com>
2026-03-09 15:53:13 +0530

    Create EXP 3 Act 3.py

---
+++ Expt4/EXP 3 Act 3.py
@@ -0,0 +1,15 @@
+# Reverse a customer feedback message.
+"""
+Created on Mon Mar  9 15:50:09 2026
+
+@author: swapnil
+"""
+
+
+
+feedback = "Best Place"
+
+# join() merges the characters back together
+reversed_feedback = "".join(reversed(feedback))
+
+print(reversed_feedback)
