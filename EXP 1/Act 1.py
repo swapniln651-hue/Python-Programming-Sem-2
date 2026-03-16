23f83136c7d8aa26143a5940e17ae02da373ca7c
swapniln651-hue <swapniln651@gmail.com>
2026-03-16 15:00:23 +0530

    Create Act 1.py

---
+++ EXP 1/Act 1.py
@@ -0,0 +1,17 @@
+num1=int(input("Enter Number1:"))
+num2=int(input("Enter Number2:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+sub=num1-num2
+print("Subraction=",sub) 
+
+mult=num1*num2
+print("Multiplication=",mult)
+
+div=num1/num2
+print("Division=",div)
+
+mod=num1%num2
+print("Modulus=",mod)
