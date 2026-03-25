f10c4c31214f052d335130dc4e2306faaa651e4d
swapniln651-hue <swapniln651@gmail.com>
2026-03-25 16:24:33 +0530

    Create EXP 3 Act 3.py

---
+++ EXP 3/EXP 3 Act 3.py
@@ -0,0 +1,14 @@
+#Code for EMI Calculation
+"""
+Created on Mon Feb 16 15:58:11 2026
+
+@author: swapnil
+"""
+
+principal=float(input("Enter principal amount:"))
+monthlyintrest=float(input("Enter monthly intrest:"))
+year=float(input("Enter loan tenure (in years):"))
+months = year * 12
+monthlyintrest = monthlyintrest /100
+emi= (principal * monthlyintrest * (1 + monthlyintrest)** months) / ((1 + monthlyintrest))
+print("your emi is:", round(emi, 2))
